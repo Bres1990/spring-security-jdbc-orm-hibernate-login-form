9f73bb9b1cbf412cb67c63a9eb271572fde853ee
Adam Poterałowicz <181710@student.pwr.edu.pl>
2016-08-15 02:30:41 +0200

    dir cleanup

--- src/main/java/com/bres/siodme/config/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.bres.siodme.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by Adam on 2016-08-14.
- */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
